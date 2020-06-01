c706ff92774738a86f60fe4472a633f645902280
nolyoi <nolan@moonship.cc>
2020-05-31 19:51:46 -0500

    Fixed Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_023615) do
+ActiveRecord::Schema.define(version: 2020_05_31_020627) do
+
+  create_table "check_ins", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "location_id"
+    t.integer "count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "location_managers", force: :cascade do |t|
     t.integer "location_id"
