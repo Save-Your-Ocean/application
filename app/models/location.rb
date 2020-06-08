86b303560235b25ff734612eb4896d80fa6645d9
nolyoi <nolan@moonship.cc>
2020-06-08 08:16:47 -0500

    Added Missing Associations

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
   extend Geocoder::Model::ActiveRecord
   
   has_many :location_managers
+  has_many :comments
+  has_many :check_ins
   has_many :users, through: :location_managers, source: :user
   validates_presence_of :name, :address, :city, :country
   geocoded_by :address
@@ -10,5 +12,5 @@ class Location < ActiveRecord::Base
   def address
     [street, city, country].compact.join(', ')
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,18 @@ class User < ActiveRecord::Base
   has_secure_password
   # has_secure_password :recovery_password
   has_many :location_managers
-  has_many :comments, through: :locations
+  has_many :check_ins
+  has_many :comments, through: :location
   has_many :managed_locations, through: :location_managers, source: :location
   validates_presence_of :username, :password, :confirmation =>true
   validates :username, uniqueness: true
   belongs_to :role
 
+  def comments 
+    Comment.where(user_id: self.id).order(created_at: :DESC)
+  end
+
+  # def checkins
+  #   CheckIn.where(location_id: self.id)
+  # end
 end
