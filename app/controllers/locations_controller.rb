f5fe25e972cbf18ec55b9611a16f7cf5e6dca667
nolyoi <nolan@moonship.cc>
2020-06-08 08:15:13 -0500

    Refactored locations profile

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,7 +14,11 @@ class LocationsController < ApplicationController
 
   # GET: /locations/new
   get "/locations/new" do
-    erb :"/locations/new"
+    if logged_in?
+      erb :"/locations/new"
+    else
+      redirect to '/users/login'
+    end
   end
 
   # POST: /locations
@@ -33,45 +37,78 @@ class LocationsController < ApplicationController
   get "/locations/pending" do
     settings.page_title = 'Pending'
     @user = current_user
-    @locations = Location.where(approved: nil)
-    if !logged_in?
-      redirect to '/users/login'
-    else
+
+    if logged_in?
+      @manager = LocationManager.all
+      @locations = Location.where(approved: nil)
+
       erb :'/locations/pending'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  post "/locations/pending/:id" do
+    settings.page_title = 'Pending'
+    @user = current_user
+  
+    if logged_in?
+      @locations = Location.where(approved: nil)
+      approve = params[:approved]
+      approval = Location.find(params[:id])
+      approval.approved = params[:approved]
+      
+      if approval.approved?
+        approval.geocode
+      end
+      approval.save
+
+      erb :'/locations/pending'
+    else
+      redirect to '/users/login' 
     end
   end
 
   # GET: /locations/5
   get "/locations/:id" do
-    @user = current_user
     @location = Location.find(params[:id])
-    @checkins = CheckIn.where(location_id: @location.id)
-    @user_checkin = CheckIn.where(user_id: @user.id, location_id: params[:id])
 
+    if logged_in?
+      erb :'/locations/show'
+    else
+      erb :'/locations/show'
+    end
     erb :'/locations/show'
   end
 
   post "/locations/:id" do
     @user = current_user
-    checkin = params[:checkin]
-    @location = Location.find(params[:id])
-    @checkins = CheckIn.where(location_id: @location.id)
-    @user_checkin = CheckIn.where(user_id: @user.id, location_id: params[:id])
-    @checkin = CheckIn.find_or_create_by(user_id: @user.id, location_id: params[:id])
-    if checkin == "true" and @checkin.count == nil
-      @checkin.count = 1
-      @checkin.save
-      flash[:success] = "You have successfully checked in to #{@location.name}! This is your first check-in at this location! Keep coming back!"
-      erb :'/locations/show'
-    elsif checkin == "true" and @checkin.count > 0
-      @checkin.count = @checkin.count + 1
-      @checkin.save
-      flash[:success] = "You have successfully checked in to #{@location.name}! You have saved <strong>#{@checkin.count}</strong> plastic bottles! "
+
+    if logged_in?
+      checkin = params[:checkin]
+      @location = Location.find(params[:id])
+      @checkins = CheckIn.where(location_id: @location.id)
+      @user_checkin = CheckIn.where(user_id: @current_user.id, location_id: params[:id])
+      @checkin = CheckIn.find_or_create_by(user_id: @current_user.id, location_id: params[:id])
+      @comment = Comment.create(location_id: @location.id, user_id: @current_user.id, body: params[:body])
+
+      if checkin == "true" and @checkin.count == nil
+        @checkin.count = 1
+        @checkin.save
+        flash[:success] = "You have successfully checked in to #{@location.name}! This is your first check-in at this location! Keep coming back!"
+        erb :'/locations/show'
+      elsif checkin == "true" and @checkin.count > 0
+        @checkin.count = @checkin.count + 1
+        @checkin.save
+        flash[:success] = "You have successfully checked in to #{@location.name}! You have saved <strong>#{@checkin.count}</strong> plastic bottles! "
+        erb :'/locations/show'
+      end
+        
       erb :'/locations/show'
     else
-      
-      erb :'/locations/show'
+      redirecto to '/users/login'
     end
+
   end
 
   # GET: /locations/5/edit
