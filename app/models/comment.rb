719c0dc5a7d3ffc216059c492fa13118069fe509
nolyoi <nolan@moonship.cc>
2020-06-08 08:18:05 -0500

    Added pagination & added user method.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :location
   validates_presence_of :body
+
+  self.per_page = 10
+
+  def user 
+    user = User.find_by(id: self.user_id)
+  end
 end
