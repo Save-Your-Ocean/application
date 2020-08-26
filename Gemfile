84044c91d50df864f84c6c2b5bb07255f19f43e1
Nolan Mayersky <n.mayersky@hey.com>
2020-08-25 20:00:47 -0500

    Update Capistrano.

--- Gemfile
+++ Gemfile
@@ -35,5 +35,8 @@ group :test do
 end
 
 group :development do
-  gem "capistrano", "~> 3.10", require: false
+  gem 'capistrano', '~> 3.11'
+  gem 'capistrano-rails', '~> 1.4'
+  gem 'capistrano-passenger', '~> 0.2.0'
+  gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
