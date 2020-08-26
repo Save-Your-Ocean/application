e1663bafbbe3ebdd408b690615a48ba7c3694a12
Nolan Mayersky <n.mayersky@hey.com>
2020-08-26 12:48:02 -0500

    Update db config.

--- config/environment.rb
+++ config/environment.rb
@@ -13,14 +13,7 @@ Dir['./config/initializers/*.rb'].sort.each { |file| require file }
 # Connect to Postgres cluster
 rack_env = ENV['APP_ENV']
 
-if ENV['APP_ENV'] == "production"
-  ActiveRecord::Base.establish_connection(
-    ENV['DB_URL']
-  )
-else
-  DB_CONFIGS = YAML.safe_load(ERB.new(File.read("./config/database.yml")).result)
-  ActiveRecord::Base.establish_connection(DB_CONFIGS[rack_env])
-end
+ActiveRecord::Base.establish_connection(ENV['DB_URL'])
 
 require 'json'
 require 'rack-flash'
